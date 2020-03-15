d601ffb34fd34ce58bc0af6f8c5628293e524e98
chanwkkk <chanwk18@hotmail.com>
2020-03-15 12:24:05 -0600

    change create route in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,5 +9,14 @@ class UsersController < ApplicationController
         end
     end
 
+    def create
+        user = User.new(user_params)
+        if user
+            render json: user
+        else
+            render json: {error: 'Error creating account'}
+        end
+    end
+
 
 end
