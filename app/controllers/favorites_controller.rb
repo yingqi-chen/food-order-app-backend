fd43dfb955a01555937f699a902db45444d6f241
chanwkkk <chanwk18@hotmail.com>
2020-03-16 15:19:26 -0600

    set favorite controller

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,8 +1,40 @@
 class FavoritesController < ApplicationController
+    before_action :set_user
     
-    
+
     def index
-        favorites = User.find(params[:user_id]).favorites
+        favorites = @user.favorites
         render json: favorites
     end
+
+    def show
+        favorite = Favorite.find(params[:id])
+        render json: favorite
+      end
+    
+    def create
+        favorite = @user.favorites.new(favorite_params)    
+        favorite.date = DateTime.now
+        favorite.status = "not-submitted"
+        if favorite.save
+            render json: favorite
+        else
+            render json: {error: "Error creating favorites."}
+        end
+    end
+    
+    
+      private
+    
+      def set_user
+        @user = User.find(params[:user_id])
+      end
+    
+    
+      def favorite_params
+        params.require(:favorite).permit(:user_id, :date, :dishes=>[
+            :name,
+            :image
+        ])
+      end
 end
