e9c74f50dd1e2fcf1f698ac6d4854bf4cae63779
chanwkkk <chanwk18@hotmail.com>
2020-03-15 19:00:56 -0600

    add user_params to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,5 +18,10 @@ class UsersController < ApplicationController
         end
     end
 
+    private
+
+    def user_params
+      params.require(:user).permit(:name, :password, :email)
+    end
 
 end
