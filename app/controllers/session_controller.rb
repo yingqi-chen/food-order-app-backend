09cd0b0bc0afcc7820ab1127c153289bb0253f6a
chanwkkk <chanwk18@hotmail.com>
2020-03-19 01:05:28 -0600

    make sure once login backend is rendering user information included orders and favorite dishes

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,9 +1,9 @@
 class SessionController < ApplicationController
     def login
-        @user = User.find_by :email=>params[:email]
-        if @user && @user.authenticate(params[:password])
-            session[:user_id] = @user.id
-            render json: @user
+        user = User.find_by :email=>params[:email]
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            render json: user, include: ['order', 'favorites','orders.dishes','favorites.dishes']
         else
             render json: {status: "error", message: "We don't find such an user according to your information,please try again."}
         end
