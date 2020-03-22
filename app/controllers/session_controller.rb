db82785b3d1d2fcce7038f90237ed4dab67454c3
chanwkkk <chanwk18@hotmail.com>
2020-03-22 17:41:34 -0600

    modify loginUser so as to remove the thunk logic from within class LoginForm

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -2,8 +2,12 @@ class SessionController < ApplicationController
     def login
         user = User.find_by :email=>params[:email]
         if user && user.authenticate(params[:password])
-            session[:user_id] = user.id
-            render json: user, include: ['order', 'favorites','orders.dishes','favorites.dishes']
+            payload = user.id
+            token = encode_token(payload)
+            render json: {
+                user:user, include: ['order', 'favorites','orders.dishes','favorites.dishes'], 
+                jwt:token
+            }
         else
             render json: {status: "error", message: "We don't find such an user according to your information,please try again."}
         end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,10 @@ class UsersController < ApplicationController
         if user.save
             payload = user.id
             token = encode_token(payload)
-            render json: {user:user, jwt:token}
+            render json: {
+                user:user, include: ['order', 'favorites','orders.dishes','favorites.dishes'] 
+                jwt:token
+            }
         else
             render json: {error: 'Error creating account'}
         end
