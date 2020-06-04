17491fe10cee5fce9479ab7bfb39d3d3735e345c
chanwkkk <chanwk18@hotmail.com>
2020-06-04 14:43:06 -0600

    config postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_03_27_223739) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "dishes", force: :cascade do |t|
     t.string "name"
     t.string "image"
